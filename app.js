4cbeb212aab6321f99db5ef1df3382b7098fce60
devry12 <devrykawiryan@gmail.com>
2020-07-22 15:35:43 +0700

    first commit

--- app.js
+++ app.js
@@ -7,12 +7,6 @@ const unitRoutes = require('./api/routes/unit')
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use('/',(req,res) =>{
-    res.json({
-        status: true,
-        message: 'runnning',
-    })
-})
 app.use('/auth', authRoutes);
 app.use('/products', productRoutes);
 app.use('/category', categoryRoutes);
