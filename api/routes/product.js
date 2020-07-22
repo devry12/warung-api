05faa2fcc024bdf10dd91d774d9e3e8b1ca2d9c6
devry12 <devrykawiryan@gmail.com>
2020-07-22 17:46:56 +0700

    first commit

--- api/routes/product.js
+++ api/routes/product.js
@@ -4,7 +4,9 @@ const router = express.Router();
 const db = require('../config/db.js')
 
 router.get('/:category_id', async (req, res, next) => {
-    var product = await db('products').where('category_id', req.params.category_id)
+    var product = await db.select('p.*', 'u.name as unit', 'c.name as category').from({ p: 'products' }).where('p.category_id', req.params.category_id).leftJoin({ u: 'unit' }, 'p.unit_id', 'u.unit_id').leftJoin({ c: 'category' }, 'p.category_id', 'c.category_id').orderBy('p.name')
+    console.log(product.length);
+
     res.json({
         status: true,
         message: 'success',
@@ -41,8 +43,8 @@ router.post('/', async (req, res, next) => {
 
 //update data product
 router.put('/', async (req, res, next) => {
-    var product = await db('products').where('product_id', req.body.product_id).update({ 
-        name: req.body.name, 
+    var product = await db('products').where('product_id', req.body.product_id).update({
+        name: req.body.name,
         name: req.body.name,
         unit_id: req.body.unit_id,
         quantity: req.body.quantity,
